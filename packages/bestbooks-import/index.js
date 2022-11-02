cf8caa180dfa6f1ddb414b37f46e9f7620f5ce47
Patrick Ingle <phkcorp2005@gmail.com>
2022-11-02 01:06:21 -0400

    v1.0.0

---
+++ packages/bestbooks-import/index.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const code = require("@patrick-ingle/bestbooks-core");
+
+async function import_csv() {
+    return "NOT IMPLEMENTED";
+}
+
+async function import_pdf() {
+    return "NOT IMPLEMENTED";
+}
+
+async function import_from_wordpress() {
+    return "NOT IMPLEMENTED";
+}
+
+async function import_from_waveaccounting() {
+    return "NOT IMPLEMENTED";
+}
+
+async function import_from_quickbooks() {
+    return "NOT IMPLEMENTED";
+}
+
+module.exports = {
+    import_csv,
+    import_pdf,
+    import_from_wordpress,
+    import_from_waveaccounting,
+    import_from_quickbooks
+}
