b33ae4f1933c20d8cbe9f98695dbbdaa2533a70c
PIPNGLEWARE <maintainers@pingleware.work>
2023-02-06 13:26:05 -0500

    implemented import CSV file

--- packages/bestbooks-import/index.js
+++ packages/bestbooks-import/index.js
@@ -1,25 +1,61 @@
 "use strict"
 
-const code = require("@pingleware/bestbooks-core");
+const { addTransaction } = require("@pingleware/bestbooks-helpers");
 
-async function import_csv() {
-    return "NOT IMPLEMENTED";
+/**
+ * 
+ * @param {object} params {filename, comapny_id, office_id} 
+ */
+function import_csv(params) {
+    const parse = require('csv-parse')
+    const fs = require('fs') 
+    
+    const data = []
+    fs.createReadStream(params.filename)
+        .pipe(parse({ delimiter: ',' }))
+        .on('data', (r) => {
+            console.log(r);
+            data.push(r);        
+        })
+        .on('end', () => {
+            return importTransactions(data,params.company_id,params.office_id);
+        });
 }
 
-async function import_pdf() {
-    return "NOT IMPLEMENTED";
+async function import_pdf(params) {
+    return await importTransactions(params);
 }
 
-async function import_from_wordpress() {
-    return "NOT IMPLEMENTED";
+async function import_from_wordpress(params) {
+    return await importTransactions(params);
 }
 
-async function import_from_waveaccounting() {
-    return "NOT IMPLEMENTED";
+async function import_from_waveaccounting(params) {
+    return await importTransactions(params);
 }
 
-async function import_from_quickbooks() {
-    return "NOT IMPLEMENTED";
+async function import_from_quickbooks(params) {
+    return await importTransactions(params);
+}
+/**
+ * 
+ * @param {array} import_data [date,description,debit,credit,account,type]
+ * @param {number} company_id 
+ * @param {number} office_id 
+ */
+function importTransactions(import_data,company_id=0,office_id=0) {
+    var date = new Date(import_data[0]).toISOString();
+    var description = import_data[1].replace(/"|'/g, '');
+    description = description.replace(/"|\\/g, '');
+    var debit = import_data[2].replace(/"|,/g, '');
+    var credit = import_data[3].replace(/"|,/g, '');
+    var account = import_data[4];
+    var type = import_data[5];
+
+    addTransaction(account,type,date,description,debit,credit,
+        function(results){
+            return results;          
+        },company_id,office_id);
 }
 
 module.exports = {
