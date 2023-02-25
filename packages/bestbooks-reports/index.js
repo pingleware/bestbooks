f388d39a001187372fbd800a9655f579ea34c568
PIPNGLEWARE <maintainers@pingleware.work>
2023-02-25 18:15:18 -0500

    trial-balance report

--- packages/bestbooks-reports/index.js
+++ packages/bestbooks-reports/index.js
@@ -1,5 +1,40 @@
 "use strict"
 
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+function init() {
+    
+    const xslt_list = [
+        "balance-sheet.xslt",
+        "customer-estimate.xslt",
+        "income-statement.xslt",
+        "noted-to-financial-statements.xslt",
+        "purchase-order.xslt",
+        "statement-in-change-in-equity.xslt",
+        "statement-of-cash-flows.xslt",
+        "trial-balance.xslt"
+    ];
+    let copied = 0;
+
+    xslt_list.forEach(function(file){
+        if (!fs.existsSync(path.join(os.homedir(),`.bestbooks/${file}`))) {
+            try {
+                fs.copyFileSync(path.join('xslt',file),path.join(os.homedir(),`.bestbooks/${file}`));
+                copied++;    
+            } catch(error) {
+                console.error(error);
+            }
+        }    
+    });    
+
+    return copied;
+}
+function copy(src,dest) {
+    fs.copyFileSync(path.join('xslt',src),path.join(os.homedir(),`.bestbooks/${dest}`));
+}
+
 const BalanceSheet = require('./balance-sheet');
 const IncomeStatement = require('./income-statement');
 const NoteToFinancialStatements = require('./noteToFinancialStatements');
@@ -9,6 +44,8 @@ const TrialBalance = require('./trial-balance');
 const RetainedEarnings = require('./retained-earnings');
 
 module.exports = {
+    init,
+    copy,
     BalanceSheet,
     IncomeStatement,
     TrialBalance,
