d4263fa7e64b4af6623235c785bd8b08ed329b52
PIPNGLEWARE <maintainers@pingleware.work>
2023-02-26 22:19:57 -0500

    added export_doc

--- packages/bestbooks-export/index.js
+++ packages/bestbooks-export/index.js
@@ -16,7 +16,45 @@ async function export_xblr() {
 
 }
 
+function export_doc(divHTML, title='', filename = ''){
+    var preHtml = `<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>${title}</title></head><body>`;
+    var footer = `<div style='mso-element:footer' id=f1><p class=MsoFooter>Created using @presspage/common-js from https://www.npmjs.com/package/@presspage/common-js</p></div>`;
+    var postHtml = "</body></html>";
+    var html = preHtml+divHTML+footer+postHtml;
+
+    var blob = new Blob(['\ufeff', html], {
+        type: 'application/msword'
+    });
+    
+    // Specify link url
+    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+    // Specify file name
+    filename = filename?filename+'.doc':'document.doc';
+    
+    // Create download link element
+    var downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob ){
+        navigator.msSaveOrOpenBlob(blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = url;
+        
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
+    
+    document.body.removeChild(downloadLink);
+}
+
 module.exports = {
     export_csv,
-    export_xblr
+    export_xblr,
+    export_doc
 }
