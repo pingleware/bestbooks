bcc433dc773e716bccbe848fb56f7b2d98d75965
PIPNGLEWARE <maintainers@pingleware.work>
2023-04-26 11:18:16 -0400

    added multiple crypto wallet account creation test

--- packages/bestbooks-core/test/5-digitalcurrency.test.js
+++ packages/bestbooks-core/test/5-digitalcurrency.test.js
@@ -12,6 +12,11 @@ describe("testing digitial currency assets",function(){
         var coa = new ChartOfAccounts();
         coa.add("Ethereum",AccountTypes.DigitalCurrency,1);
     })
+    it("create an account for multiple ethereum wallets", function(){
+        var coa = new ChartOfAccounts();
+        // Add the wallet address with the currency type
+        coa.add("Ethereum 0x50542cF0903152E1761cffF01d2928C6F229D678",AccountTypes.DigitalCurrency,1);
+    })
     it("make a deposit to bitcoin", function(){
         var bitcoin = new Bitcoin("1FjvkiYbcFiNJdM57z7Bj8LJcKRCjUqrM9") // Bitcoin Wallet Address with CashApp for the framework creator
         bitcoin.addDebit("2021-10-24 18:24","CashApp Bitcoin Boost paid to 1FjvkiYbcFiNJdM57z7Bj8LJcKRCjUqrM9",1.15,1,1);
