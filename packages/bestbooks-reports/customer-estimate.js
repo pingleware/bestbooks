78c47f65b372a5c2f9c68a3aa23b082b2a788a46
PIPNGLEWARE <maintainers@pingleware.work>
2024-04-07 10:29:28 -0400

    formatted decimals numbers

--- packages/bestbooks-reports/customer-estimate.js
+++ packages/bestbooks-reports/customer-estimate.js
@@ -11,7 +11,26 @@ class CustomerEstimate {
     }
 
     createReport(customerEstimateInfo,callback) {
+        var lineItems = [];
+        customerEstimateInfo.lineitems.lineitem.forEach(function(lineItem){
+            lineItems.push({
+                quantity: lineItem.quantity,
+                description: lineItem.description,
+                unitprice: Number(lineItem.unitprice).toFixed(2),
+                discount: Number(lineItem.discount).toFixed(2),
+                tax: Number(lineItem.tax).toFixed(2),
+                total: Number(lineItem.total).toFixed(2)
+            })
+        })
+        customerEstimateInfo.lineitems = { lineitem: lineItems };
+
+        customerEstimateInfo.prices.subtotal = Number(customerEstimateInfo.prices.subtotal).toFixed(2);
+        customerEstimateInfo.prices.tax = Number(customerEstimateInfo.prices.tax).toFixed(2);
+        customerEstimateInfo.prices.shipping = Number(customerEstimateInfo.prices.shipping).toFixed(2);
+        customerEstimateInfo.prices.other = Number(customerEstimateInfo.prices.other).toFixed(2);
+        customerEstimateInfo.prices.total = Number(customerEstimateInfo.prices.total).toFixed(2);
         var formattedData = array2xml('customerEstimate',customerEstimateInfo);
+        //console.log(formattedData)
         fs.writeFileSync(path.join(os.homedir(),'.bestbooks/customer-estimate.xml'), formattedData);
         // Save report XML data to report table
         var txdate = new Date().getTime();
