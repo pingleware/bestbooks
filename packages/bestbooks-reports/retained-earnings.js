d52a0a0f922ebe17449993601faa36224b0844a0
PINGLEWARE <presspage.entertainment@gmail.com>
2024-11-10 15:26:57 -0500

    add retained earnings

--- packages/bestbooks-reports/retained-earnings.js
+++ packages/bestbooks-reports/retained-earnings.js
@@ -1,110 +1,70 @@
 "use strict"
 
-const { Model } = require('@pingleware/bestbooks-core');
 const BaseReport = require('./report');
 const {format, array2xml} = require('./formatReport');
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
 
-class RetainedEarnings extends BaseReport {
+class RetainedEarningsReport extends BaseReport {
     constructor() {
         super();
     }
 
-    createReport(startDate,endDate,_format,callback,notes="") {
-        this.retrieveReportData(startDate, endDate, function(data){
-            if (_format == "array") {
-                var _data = {
-                    date: new Date().toDateString(),
-                    previous_retained_earnings: Number(data[0].previous_retained_earnings).toFixed(2),
-                    net_income: Number(data[0].net_income).toFixed(2),
-                    retained_earnings: Number(data[0].retained_earnings).toFixed(2),
-                    notes: notes
-                };
-                var formattedData = array2xml('retainedEarnings',_data);
-                fs.writeFileSync(path.join(os.homedir(),'.bestbooks/retained-earnings.xml'), formattedData);
-                // Save report XML data to report table
-                var txdate = new Date().getTime();
-                var buffer = require('buffer').Buffer;
-                // TODO: move this INSERT in the Core.Report class per CODING STANDARDS
-                var sql = `INSERT INTO report (txdate,name,contents) VALUES ('${txdate}','retained-earnings','${buffer.from(formattedData).toString('base64')}')`;
-                const model = new Model();
-                if (callback) {
-                    model.insert(sql,function(result){
-                        callback(format("retainedEarnings",formattedData));
-                    });
-                } else {
-                    model.insertSync(sql);
-                    return format("retainedEarnings",formattedData);
-                }
-            } else {
-                // process other formats
-            }    
-        });
+    saveReport(name, contents, type="xml",callback=null){
+        const filePath = path.join(os.homedir(),`.bestbooks/${name}.${type}`);
+        // the xslt-processor does not support the XSLT syntax xsl:for-each-group, 
+        // so the XML generated is returned,
+        // using a free tool like https://xslttest.appspot.com/, 
+        // to copy the .bestbooks/balance-sheet.xslt and .bestbooks/balance-sheet.xml
+        // to render a HTML
+        // TODO: implement xsl:for-each-group. callback(format("balanceSheet",formattedData));
+
+        fs.writeFileSync(filePath, contents);
+        callback(filePath)
     }
 
+    async saveReportSync(name, html, type) {
+        return new Promise((resolve,reject) => {
+            try {
+                this.saveReport(name, html, type, function(filePath){
+                    resolve(filePath);
+                })    
+            } catch(error) {
+                reject(error);
+            }
+        })
+    }
+
+    async formatHtml(formattedData) {
+        return format('retainedEarnings',formattedData);
+    }
+
+    async formatXml(contents) {
+        return array2xml('retainedEarnings',contents);
+    }
+
+    async formatArray(data, notes) {
+        var _data = {
+            date: new Date().toDateString(),
+            previous_retained_earnings: Number(data[0].beginning_retained_earnings).toFixed(2),
+            net_income: Number(data[0].net_income).toFixed(2),
+            dividends_paid: Number(data[0].dividends_paid).toFixed(2),
+            retained_earnings: Number(data[0].ending_retained_earnings).toFixed(2),
+            notes: notes
+        };
+
+        return _data;
+    }
+
+
     retrieveReportData(startDate,endDate,callback) {
-        var _startDate = startDate;
-        var lastYear, lastStartDate, lastEndDate;
-        if (_startDate.length == 0) {
-            lastYear = Number(new Date().getFullYear() - 1);
-        } else {
-            lastYear = Number(new Date(startDate).getFullYear() - 1);
-        }
-        lastStartDate = new Date(`01-01-${lastYear}`).toISOString().split('T')[0];
-        lastEndDate = new Date(`12-31-${lastYear}`).toISOString().split('T')[0];
-
-        var sql = `SELECT 
-            (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%income%" OR account_name LIKE "%revenue%") AS net_income,
-            (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%dividend%") AS net_dividend;
-        `;
-        if (startDate.length > 0 && endDate.length > 0) {
-            sql = `SELECT 
-                (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%income%" OR account_name LIKE "%revenue%" WHERE txdate BETWEEN ${startDate} AND ${endDate}) AS net_income,
-                (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%dividend%" WHERE txdate BETWEEN ${startDate} AND ${endDate}) AS net_dividend;
-            `;
-        } else if (startDate.length > 0 && endDate.length == 0) {
-            sql = `SELECT 
-                (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%income%" OR account_name LIKE "%revenue%" WHERE txdate >= ${startDate}) AS net_income,
-                (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%dividend%" WHERE txdate >= ${startDate}) AS net_dividend;
-            `;
-        } else if (startDate.length == 0 && endDate.length > 0) {
-            sql = `SELECT 
-                (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%income%" OR account_name LIKE "%revenue%" WHERE txdate < ${endDate}) AS net_income,
-                (SELECT SUM(balance) AS total FROM ledger WHERE account_name LIKE "%dividend%" WHERE txdate < ${endDate}) AS net_dividend;
-            `;
-        }
-
-        //var sql = `SELECT (SELECT ROUND(SUM(debit)+SUM(credit),2) FROM ledger WHERE account_code LIKE '9%') AS net_income, 
-        //((SELECT ROUND(SUM(debit)+SUM(credit),2) FROM ledger WHERE account_code LIKE '9%') - 
-        //ROUND(SUM(debit)+SUM(credit),2)) AS retained_earnings,((SELECT ROUND(SUM(debit)+SUM(credit),2) 
-        //FROM ledger WHERE account_code LIKE '9%') - ROUND(SUM(debit)+SUM(credit),2) AND 
-        //txdate BETWEEN DATE('${lastStartDate}') AND DATE('${lastEndDate}')) AS previous_retained_earnings  
-        //FROM ledger WHERE account_name='Dividends Payable'`;
-        //if (startDate.length > 0 && endDate.length > 0) {
-        //    sql = `${sql} AND txdate BETWEEN ${startDate} AND ${endDate}`;
-        //} else if (startDate.length > 0 && endDate.length == 0) {
-        //    sql = `${sql} AND txdate >= ${startDate}`;
-        //} else if (startDate.length == 0 && endDate.length > 0) {
-        //    sql = `${sql} AND txdate < ${endDate}`;
-        //}
-        // TODO: move this INSERT in the Core.Report class per CODING STANDARDS
-        const model = new Model();
-        model.query(sql,callback);
+        callback(this.retrieveReportDataSync(startDate,endDate));
     }
 
     async retrieveReportDataSync(startDate,endDate) {
-        return new Promise((resolve, reject) => {
-            this.retrieveReportData(startDate, endDate, function(err, results) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(results);
-                }
-            });
-        });
+        return this.statementOfRetainedEarningsSync(startDate,endDate);
     }
 }
 
-module.exports = RetainedEarnings;
+module.exports = RetainedEarningsReport;
--- packages/bestbooks-reports/test/retainedearnings.test.js
+++ packages/bestbooks-reports/test/retainedearnings.test.js
@@ -1,18 +1,30 @@
 const assert = require('assert'); 
 const { 
     init, 
-    RetainedEarnings, 
+    RetainedEarningsReport, 
 } = require("../index");
+const {
+    RetainedEarnings,
+    Equity,
+    Revenue,
+    Expense,
+} = require('@pingleware/bestbooks-core');
 const fs = require('fs');
+const path = require('path');
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 describe('Create formatted Retained Earnings Report',function(){
-    let report;
+    let report, retainedEarnings, commonStock, revenue, expense, data, formattedData, xml;
 
-    before(function(){
+    before(function(done){
+        report = new RetainedEarningsReport();
+        retainedEarnings = new RetainedEarnings();
+        commonStock = new Equity("Common Stock");
+        revenue = new Revenue("Revenue");
+        expense = new Expense("Expense");
         init();
-        report = new RetainedEarnings();
+        done();
     })
 
     beforeEach(async function() {
@@ -28,8 +40,82 @@ describe('Create formatted Retained Earnings Report',function(){
         await report.model.insertSync(`UPDATE sqlite_sequence SET seq=0 WHERE name='accounts';`);
     })
 
-    it("should create an instance of RetainedEarnings", async function(){
-        assert.ok(report instanceof RetainedEarnings);
+    it("should create an instance of RetainedEarningsReport", async function(){
+        assert.ok(report instanceof RetainedEarningsReport);
     }) 
 
+    it('should add opening balance entry to retained earnings',async() => {
+        const [ledger_id,journal_id] = await retainedEarnings.addCredit("2024-10-01","Opening Balance",1000);
+        assert.equal(ledger_id,1);
+        assert.equal(journal_id,1);
+    })
+
+    it('should add owner contributions entry to retained earnings',async() => {
+        const [ledger_id,journal_id] = await retainedEarnings.addCredit("2024-10-02","Owner Contribution",200);
+        assert.equal(ledger_id,2);
+        assert.equal(journal_id,2);
+    })
+
+    it('should add dividends entry to retained earnings',async() => {
+        const [ledger_id,journal_id] = await retainedEarnings.addDebit("2024-10-03","Dividends",300);
+        assert.equal(ledger_id,3);
+        assert.equal(journal_id,3);
+    })
+
+    it('should add other adjustment entry to retained earnings',async() => {
+        const [ledger_id,journal_id] = await retainedEarnings.addCredit("2024-10-04","Other Adjustment",100);
+        assert.equal(ledger_id,4);
+        assert.equal(journal_id,4);
+    })
+
+    it('should add revenue credit entry',async() => {
+        const [ledger_id,journal_id] = await revenue.addCredit("2024-10-05","Revenue",500);
+        assert.equal(ledger_id,5);
+        assert.equal(journal_id,5);
+    })
+
+    it('should add expense debit entry',async() => {
+        const [ledger_id,journal_id] = await expense.addDebit("2024-10-05","Expense",100);
+        assert.equal(ledger_id,6);
+        assert.equal(journal_id,6);
+    })
+
+    it('should add common stock entry for owner contributions',async() => {
+        const [ledger_id,journal_id] = await commonStock.addDebit("2024-10-02","Owner Contribution",200);
+        assert.equal(ledger_id,7);
+        assert.equal(journal_id,7);
+    })
+
+    it('should return the retained earnings',async function(){
+        data = await report.retrieveReportDataSync(); 
+        //assert.equal(rows.length,2);
+        console.log(data)
+    })
+
+    it("should format data into array",async() => {
+        const notes = `In our opinion, the balance sheet presents fairly, in all material respects, the financial position as of the date specified in accordance with FASB ASC Topic 505 - Equity.`;
+
+        formattedData = await report.formatArray(data,notes);
+        //const expected = `{"date":"${new Date().toDateString()}","lineItems":{"assets":[{"code":"100","name":"Cash","balance":"1000.00","type":"Asset"}],"liabilities":[{"code":"200","name":"COGS","balance":"350.00","type":"Liability"}],"expenses":[],"income":[],"equity":[]},"totalAsset":"1000.00","totalLiability":"350.00","totalIncome":"0.00","totalExpense":"0.00","totalEquity":"0.00","totalLiabilitiesShareholdersEquity":"350.00","notes":"In our opinion, the balance sheet presents fairly, in all material respects, the financial position as of the date specified in accordance with FASB ASC Topic 210, Balance Sheet."}`;
+        //assert.strictEqual(JSON.stringify(formattedData).trim(),expected.trim())
+        console.log(formattedData)
+    })
+
+    it("should format array into xml",async () => {
+        xml = await report.formatXml(formattedData);
+    });
+
+    it("should save the xml to a file",async() => {
+        const filePath = await report.saveReportSync("retainedEarnings",xml,"xml");
+        assert.strictEqual(path.basename(filePath),"retainedEarnings.xml");
+    })
+
+    it("should format the retained earnings report",async() => {
+        html = await report.formatHtml(xml);
+    })
+
+    it("should save the html to a file",async function(){
+        const filePath = await report.saveReportSync("retainedEarnings",html,"html");
+        assert.strictEqual(path.basename(filePath),"retainedEarnings.html");
+    })
 })
