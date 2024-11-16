224d847e5dc6e27f9de7fecf31066d3f4ad4cf11
PINGLEWARE <presspage.entertainment@gmail.com>
2024-11-15 19:30:44 -0500

    refactor to inherit BaseReport for consistency

--- packages/bestbooks-reports/customer-estimate.js
+++ packages/bestbooks-reports/customer-estimate.js
@@ -1,52 +1,104 @@
 "use strict"
 
-const { Model } = require('@pingleware/bestbooks-core');
+const BaseReport = require('./report');
 const {format, array2xml} = require('./formatReport');
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
 
-class CustomerEstimate {
+class CustomerEstimate extends BaseReport {
     constructor() {
+        super()
     }
 
-    createReport(customerEstimateInfo,callback) {
-        var lineItems = [];
-        customerEstimateInfo.lineitems.lineitem.forEach(function(lineItem){
-            lineItems.push({
-                quantity: lineItem.quantity,
-                description: lineItem.description,
-                unitprice: Number(lineItem.unitprice).toFixed(2),
-                discount: Number(lineItem.discount).toFixed(2),
-                tax: Number(lineItem.tax).toFixed(2),
-                total: Number(lineItem.total).toFixed(2)
-            })
+    saveReport(name, contents, type="xml",callback=null){
+        const filePath = path.join(os.homedir(),`.bestbooks/${name}.${type}`);
+        // the xslt-processor does not support the XSLT syntax xsl:for-each-group, 
+        // so the XML generated is returned,
+        // using a free tool like https://xslttest.appspot.com/, 
+        // to copy the .bestbooks/balance-sheet.xslt and .bestbooks/balance-sheet.xml
+        // to render a HTML
+        // TODO: implement xsl:for-each-group. callback(format("balanceSheet",formattedData));
+
+        fs.writeFileSync(filePath, contents);
+        callback(filePath)
+    }
+
+    async saveReportSync(name, html, type) {
+        return new Promise((resolve,reject) => {
+            try {
+                this.saveReport(name, html, type, function(filePath){
+                    resolve(filePath);
+                })    
+            } catch(error) {
+                reject(error);
+            }
         })
-        customerEstimateInfo.lineitems = { lineitem: lineItems };
-
-        customerEstimateInfo.prices.subtotal = Number(customerEstimateInfo.prices.subtotal).toFixed(2);
-        customerEstimateInfo.prices.tax = Number(customerEstimateInfo.prices.tax).toFixed(2);
-        customerEstimateInfo.prices.shipping = Number(customerEstimateInfo.prices.shipping).toFixed(2);
-        customerEstimateInfo.prices.other = Number(customerEstimateInfo.prices.other).toFixed(2);
-        customerEstimateInfo.prices.total = Number(customerEstimateInfo.prices.total).toFixed(2);
-        var formattedData = array2xml('customerEstimate',customerEstimateInfo);
-        console.log(formattedData)
-        fs.writeFileSync(path.join(os.homedir(),'.bestbooks/customer-estimate.xml'), formattedData);
-        // Save report XML data to report table
-        var txdate = new Date().getTime();
-        var buffer = require('buffer').Buffer;
-        // TODO: move this INSERT in the Core.Report class per CODING STANDARDS
-        var sql = `INSERT INTO report (txdate,name,contents) VALUES ('${txdate}','customer-estimate','${buffer.from(formattedData).toString('base64')}')`;
-        const model = new Model();
-        if (callback) {
-            model.insert(sql,function(result){
-                callback(format("customerEstimate",formattedData));
-            });
-        } else {
-            model.insertSync(sql);
-            return format("customerEstimate",formattedData);
-        }
     }
+
+    async formatHtml(formattedData) {
+        return format("customerEstimate",formattedData);
+    }
+
+    async formatXml(contents) {
+        return array2xml("customerEstimate",contents);
+    }
+
+    async formatArray(data,notes="") {
+        return data;
+    }
+
+    retrieveReportData(startDate,endDate,callback) {
+        callback(this.retrieveReportDataSync(startDate, endDate));
+    }
+
+    async retrieveReportDataSync(startDate, endDate, company_id=0, customer_id=0, metadata=null) {
+        var sql = `SELECT c.name,u.* FROM company c JOIN users u ON c.id=u.company_id WHERE c.id=?`;;
+        var params = [company_id];
+        const company = await this.model.querySync(sql,params);
+        sql = `SELECT * FROM users WHERE id=?`;
+        params = [customer_id];
+        const customer = await this.model.querySync(sql,params);
+
+        return  {
+            number: metadata.number,
+            date: metadata.date,
+            company: {
+                name: company[0].name,
+                contact: company[0].display_name, 
+                email: company[0].user_email, 
+                phone: company[0].phone, 
+                website: metadata.company.website, 
+                address1: metadata.company.address1, 
+                address2: metadata.company.address2, 
+                city: metadata.company.city, 
+                state: metadata.company.state, 
+                zipcode: metadata.company.zipcode, 
+                fax: metadata.company.fax
+            }, 
+            customer: {
+                id: customer[0].id,
+                name: customer[0].display_name,
+                email: customer[0].user_email,
+                contact: customer[0].display_name,
+                address1: metadata.customer.address1,
+                address2: metadata.customer.address2,
+                city: metadata.customer.city,
+                state: metadata.customer.state,
+                zipcode: metadata.customer.zipcode,
+                phone: customer[0].phone,
+                fax: metadata.customer.fax,
+                shipping: metadata.customer.shipping
+            }, 
+            terms: metadata.terms,
+            tax: metadata.tax,
+            duedate: metadata.duedate,
+            lineitems: metadata.lineitems, 
+            comments: metadata.comments, 
+            prices: metadata.prices,
+            payment: metadata.payment
+        }
+    };
 }
 
 module.exports = CustomerEstimate;
--- packages/bestbooks-reports/purchase-order.js
+++ packages/bestbooks-reports/purchase-order.js
@@ -1,18 +1,74 @@
 "use strict"
 
-const { Model } = require('@pingleware/bestbooks-core');
+const BaseReport = require('./report');
 const {format, array2xml} = require('./formatReport');
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
 
-class PurchaseOrder {
+class PurchaseOrder extends BaseReport {
     constructor() {
+        super()
     }
 
-    createReport(purchaseOrderInfo,callback) {
+    saveReport(name, contents, type="xml",callback=null){
+        const filePath = path.join(os.homedir(),`.bestbooks/${name}.${type}`);
+        // the xslt-processor does not support the XSLT syntax xsl:for-each-group, 
+        // so the XML generated is returned,
+        // using a free tool like https://xslttest.appspot.com/, 
+        // to copy the .bestbooks/balance-sheet.xslt and .bestbooks/balance-sheet.xml
+        // to render a HTML
+        // TODO: implement xsl:for-each-group. callback(format("balanceSheet",formattedData));
+
+        fs.writeFileSync(filePath, contents);
+        callback(filePath)
+    }
+
+    async saveReportSync(name, html, type) {
+        return new Promise((resolve,reject) => {
+            try {
+                this.saveReport(name, html, type, function(filePath){
+                    resolve(filePath);
+                })    
+            } catch(error) {
+                reject(error);
+            }
+        })
+    }
+
+    async formatHtml(formattedData) {
+        return format("purchaseOrder",formattedData);
+    }
+
+    async formatXml(contents) {
+        return array2xml("purchaseOrder",contents);
+    }
+    
+    async formatArray(data,notes) {
         var lineItems = [];
-        purchaseOrderInfo.lineitems.lineitem.forEach(function(lineItem){
+        let subtotal = 0;
+        let total = 0;
+        var _data = {
+            number: data.number,
+            date: data.date,
+            company: data.company,
+            vendor: data.vendor,
+            shipping: data.shipping,
+            lineItems: [],
+            prices: {
+                subtotal: subtotal,
+                tax: 0,
+                shipping: Number(data.prices.shipping).toFixed(2),
+                other: Number(data.prices.other).toFixed(2),
+                total: total    
+            },
+            comments: data.comments,
+            payment: data.payment,  
+            notes: notes
+        };
+
+        data.lineitems.forEach(function(lineItem){
+            subtotal += Number(lineItem.amount);
             lineItems.push({
                 quantity: lineItem.quantity,
                 description: lineItem.description,
@@ -20,37 +76,64 @@ class PurchaseOrder {
                 amount: Number(lineItem.amount).toFixed(2)
             })
         })
-        purchaseOrderInfo.lineitems = { lineitem: lineItems };
-        purchaseOrderInfo.prices.subtotal = Number(purchaseOrderInfo.prices.subtotal).toFixed(2);
-        purchaseOrderInfo.prices.tax = Number(purchaseOrderInfo.prices.tax).toFixed(2);
-        purchaseOrderInfo.prices.shipping = Number(purchaseOrderInfo.prices.shipping).toFixed(2);
-        purchaseOrderInfo.prices.other = Number(purchaseOrderInfo.prices.other).toFixed(2);
-        purchaseOrderInfo.prices.total = Number(purchaseOrderInfo.prices.total).toFixed(2);
 
-        var formattedData = array2xml('purchaseOrder',purchaseOrderInfo);
-        fs.writeFileSync(path.join(os.homedir(),'.bestbooks/purchase-order.xml'), formattedData);
-        // Save report XML data to report table
-        var txdate = new Date().getTime();
-        var buffer = require('buffer').Buffer;
-        // TODO: move this INSERT in the Core.Report class per CODING STANDARDS
-        var sql = `INSERT INTO report (txdate,name,contents) VALUES ('${txdate}','purchase-order','${buffer.from(formattedData).toString('base64')}')`;
-        const model = new Model();
-        if (callback) {
-            model.insert(sql,function(result){
-                callback(format("purchaseOrder",formattedData));
-            });
-        } else {
-            model.insertSync(sql);
-            return format("purchaseOrder",formattedData);
-        }
+        total = Number(subtotal) + (Number(subtotal) * Number(data.prices.tax)) + Number(data.prices.shipping) + Number(data.prices.other);
+
+        _data.lineItems = { lineitem: lineItems };
+        _data.prices.tax = (Number(subtotal) * Number(data.prices.tax)).toFixed(2);
+        _data.prices.subtotal = Number(subtotal).toFixed(2);
+        _data.prices.total = Number(total).toFixed(2);
+
+        return _data;
+    }
+    
+    retrieveReportData(startDate,endDate,callback) {
+        callback(this.retrieveReportDataSync(startDate, endDate));
     }
 
-    createReportFromXML(formattedData,callback) {
-        if (callback) {
-            callback(format("purchaseOrder",formattedData));
-        } else {
-            return format("purchaseOrder",formattedData);
-        }
+    async retrieveReportDataSync(startDate, endDate, company_id=0, vendor_id=0, metadata=null){
+        var sql = `SELECT c.name,u.* FROM company c JOIN users u ON c.id=u.company_id WHERE c.id=?`;;
+        var params = [company_id];
+        const company = await this.model.querySync(sql,params);
+        sql = `SELECT * FROM users WHERE id=?`;
+        params = [vendor_id];
+        const vendor = await this.model.querySync(sql,params);
+
+        return  {
+            number: metadata.number,
+            date: startDate,
+            company: {
+                name: company[0].name,
+                contact: company[0].display_name, 
+                email: company[0].user_email, 
+                phone: company[0].phone, 
+                website: metadata.company.website, 
+                billing: metadata.company.billing, 
+                shipping: metadata.company.shipping
+            }, 
+            vendor: {
+                id: 1,
+                name: vendor[0].display_name,
+                email: vendor[0].user_email,
+                contact: vendor[0].display_name,
+                address1: metadata.vendor.address1,
+                address2: metadata.vendor.address2,
+                city: metadata.vendor.city,
+                state: metadata.vendor.state,
+                zipcode: metadata.vendor.zipcode,
+                phone: metadata.vendor.phone,
+                fax: metadata.vendor.fax
+            }, 
+            shipping: {
+                service: metadata.shipping.service, 
+                method: metadata.shipping.method, 
+                deliverydate: endDate
+            },
+            lineitems: metadata.lineitems, 
+            comments: metadata.comments, 
+            prices: metadata.prices,
+            payment: metadata.payment
+        };
     }
 }
 
