e892ebd73d4d7b09a18cf5312450afc78e6c4127
PIPNGLEWARE <maintainers@pingleware.work>
2024-12-10 12:09:35 -0500

    bestbooks-api

---
+++ packages/bestbooks-api/test/main.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+describe('bestbooks-api Testing', () => {
+    let model, date, dateString;
+
+      before(async()=>{
+        date = new Date().toISOString().split("T")[0];
+        dateString = new Date().toDateString();
+        model = new Model();
+      })
+
+      beforeEach(async function() {
+        await delay(1000); // Delay of 1 second before each test
+      });
+
+      after(async()=>{
+      })
+      
+})
