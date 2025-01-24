0eca10f45130b63bc6e6454c21a2b08eb1fbd8c3
PIPNGLEWARE <maintainers@pingleware.work>
2025-01-24 11:32:09 -0500

    initial base code for UI

--- packages/bestbooks-ui/README.md
+++ packages/bestbooks-ui/README.md
@@ -1,2 +1,4 @@
 # BestBooks Accounting Application Framework - UI
 The user interface or application for interacting with the BestBooks Accounting Application Framework
+
+Initially, the UI would be developed after all other components have been completed, but a change in direction to include concurrent development to assist in validation.
---
+++ packages/bestbooks-ui/index.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const {app, BrowserWindow, ipcMain} = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+      width: 1200,
+      height: 900,
+      icon: path.join(__dirname, "assets/logo.png"),
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: true,
+        enableRemoteModule: false,
+        preload: path.join(__dirname, "preload.js")
+      }
+    });
+
+    mainWindow.loadFile("views/index.html");
+
+    mainWindow.on("closed", function () {
+      mainWindow = null;
+    });
+    mainWindow.setMenu(null);
+    mainWindow.setResizable(true);
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
